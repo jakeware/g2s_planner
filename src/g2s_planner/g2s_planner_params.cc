53a7f3ff5f6f438ae22a7b6be33ce8129a2e8677
jakeware <jakeware@gmail.com>
2017-03-27 02:13:33 -0400

    Update param file.

---
+++ src/g2s_planner/g2s_planner_params.cc
@@ -0,0 +1,11 @@
+// Copyright 2017 Massachusetts Institute of Technology
+
+#include "g2s_planner/g2s_planner_params.h"
+
+namespace g2s_planner {
+
+G2SPlannerParams::G2SPlannerParams()
+  : test_param_(false) {
+  // nothing
+}
+}  // namespace g2s_planner
---
+++ src/g2s_planner/g2s_planner_params.h
@@ -0,0 +1,11 @@
+// Copyright 2017 Massachusetts Institute of Technology
+#pragma once
+
+namespace g2s_planner {
+class G2SPlannerParams {
+ public:
+  G2SPlannerParams();
+
+  bool test_param_;
+};
+}  // namespace g2s_planner
