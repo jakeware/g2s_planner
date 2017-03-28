e9134f9b740e12a4b9e910277de6433cc9585d9b
jakeware <jakeware@gmail.com>
2017-03-28 01:11:03 -0400

    Update gs2_planner_ros.h.

--- src/g2s_planner_ros/g2s_planner_ros.h
+++ src/g2s_planner_ros/g2s_planner_ros.h
@@ -3,59 +3,40 @@
 
 #include <csignal>
 #include <memory>
-#include <string>
-#include <vector>
 
 #include "ros/ros.h"
 #include "tf2_ros/transform_listener.h"
-#include "message_filters/subscriber.h"
-#include "sensor_msgs/PointCloud2.h"
-#include "pcl_ros/point_cloud.h"
-#include "visualization_msgs/MarkerArray.h"
-#include "nav_msgs/OccupancyGrid.h"
 
-#include "global_mapper/global_mapper.h"
+#include "g2s_planner/g2s_planner.h"
 
-namespace global_mapper {
+namespace g2s_planner {
 
-class GlobalMapperRos {
+class G2SPlannerRos {
  public:
-  explicit GlobalMapperRos(volatile std::sig_atomic_t* stop_signal_ptr);
+  explicit G2SPlannerRos(volatile std::sig_atomic_t* stop_signal_ptr);
 
   void Run();
 
   volatile std::sig_atomic_t* stop_signal_ptr_;
 
  private:
-  void GetParams(GlobalMapperParams& global_mapper_params);
+  void GetParams(G2SPlannerParams& g2s_planner_params);
   void InitSubscribers();
   void InitPublishers();
-  void PopulatePixelMapMsg(nav_msgs::OccupancyGrid* occupancy_grid);
-  void PopulateVoxelMapMsg(visualization_msgs::MarkerArray* marker_array);
-  void PublishMap(const ros::TimerEvent& event);
-  std::vector<double> GrayscaleToRGBJet(double v, double vmin, double vmax);
 
   // callbacks
-  void PointCloudCallback(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr& cloud_ptr);
 
   // publishers
-  ros::Publisher pixel_map_pub_;
-  ros::Publisher voxel_map_pub_;
-  ros::Timer map_pub_timer_;
-  ros::Publisher marker_pub_;
 
   // subscribers
-  ros::Subscriber point_cloud_sub_;
 
   // params
-  GlobalMapperParams params_;
-  bool publish_pixel_map_;
-  bool publish_voxel_map_;
+  G2SPlannerParams params_;
 
-  std::unique_ptr<GlobalMapper> global_mapper_ptr_;
+  std::unique_ptr<G2SPlanner> g2s_planner_ptr_;
   ros::NodeHandle nh_;
   ros::NodeHandle pnh_;
   tf2_ros::TransformListener tf_listener_;
   tf2_ros::Buffer tf_buffer_;
 };
-}  // namespace global_mapper
+}  // namespace g2s_planner
